b1a8f796777e5c953a429f8278031d9cc17e5fb2
adse <adse@ailleron.com>
2019-09-05 00:23:06 +0200

    install heroku dependencies

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/libraryfronted'));
+app.use(express.static(__dirname + '/dist/light-bootstrap-dashboard-angular2-master'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/libraryfronted/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/light-bootstrap-dashboard-angular2-master/index.html'));
 });
 
 // default Heroku PORT
